6c078bfa8ad61da4f9d6c1623ecbb16e9f7cc351
dyedm1 <henry@hyf.one>
2023-12-18 12:03:46 -0500

    fix: prevent transfers of removedLiquidity

--- contracts/SemiFungiblePositionManager.sol
+++ contracts/SemiFungiblePositionManager.sol
@@ -616,9 +616,9 @@ contract SemiFungiblePositionManager is ERC1155, Multicall {
                 (s_accountFeesBase[positionKey_to] != 0)
             ) revert Errors.TransferFailed();
 
-            // Revert if not all balance is transferred
+            // Revert if sender has long positions in that chunk or the entire liquidity is not being transferred
             uint256 fromLiq = s_accountLiquidity[positionKey_from];
-            if (fromLiq.rightSlot() != liquidityChunk.liquidity()) revert Errors.TransferFailed();
+            if (fromLiq != liquidityChunk) revert Errors.TransferFailed();
 
             int256 fromBase = s_accountFeesBase[positionKey_from];
 
