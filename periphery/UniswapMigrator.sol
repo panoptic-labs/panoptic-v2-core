0015847897b1c012587f73e1db92889e179af8f3
Steve Flanagan <flanagan.s@husky.neu.edu>
2024-04-19 15:50:33 -0400

    WIP

---
+++ periphery/UniswapMigrator.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import {PanopticFactory} from "@contracts/PanopticFactory.sol";
+import {PanopticPool} from "@contracts/PanopticPool.sol";
+import {CollateralTracker} from "@contracts/CollateralTracker.sol";
+
+import {IUniswapV3Factory} from "v3-core/interfaces/IUniswapV3Factory.sol";
+import {INonfungiblePositionManager} from "v3-periphery/interfaces/INonfungiblePositionManager.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/// @title Facilitates the migration from Uniswap LPing to PLPing.
+contract UniswapMigrator {
+
+    PanopticFactory public panopticFactory;
+    INonfungiblePositionManager public univ3NFPM;
+    IUniswapV3Factory public univ3Factory;
+
+    constructor(PanopticFactory panopticFactory_, INonfungiblePositionManager univ3NFPM_, IUniswapV3Factory univ3Factory_) {
+        panopticFactory = panopticFactory_;
+        univ3NFPM = univ3NFPM_;
+        univ3Factory = univ3Factory_;
+    }
+
+    function migrate(uint256 tokenIds) external {
+        // PRECONDITION: the user should either setApprovalForAll(uniswapMigrator) ahead of calling this function, or approve each position NFT one-by-one
+        // otherwise it will revert. We don't require (ie:
+
+        //     require(uniswapNFPM.isApprovedForAll(msg.sender, address(this)));
+
+        // but we will revert inside the `for` on the first `decreaseLiquidity` call if unapproved.
+
+        // TODO: There's an argument that this contract doesn't have to track how much it's collecting, and could just .approve the collateral tracker for this contract's whole balance of each token.
+        // - Theoretically, this contract should start and end each call to this method with a token balance of 0 based on its own actions. The intermediate balance should only be from the `.collect` calls its making to Uniswap.
+        // - If it was receiving random tokens outside of calls to this method, they'd just be donated to the subsequent caller of this method after that random transfer, which seems fine - we advise not transferring here for that reason but can't help it if people do.
+        // However, to me, letting token transfers outside this method influence the amount we then pass into the CollateralTracker feels like it increases the attack surface area in exchange for a little gas saving.. I'd rather just track how much we're collecting and send that over:
+        uint amount0Collected;
+        uint amount1Collected;
+
+        uint128 liquidity;
+        address token0;
+        address token1;
+        for (uint i; i < tokenIds.length;) {
+            (,,token0,token1,,,,liquidity,,,,) = univ3NFPM.positions(tokenIds[i]);
+            univ3NFPM.decreaseLiquidity(
+                INonfungiblePositionManager.DecreaseLiquidityParams({
+                    tokenId: tokenIds[i],
+                    liquidity: liquidity,
+                    amount0Min: 0,
+                    amount1Min: 0,
+                    deadline: uint32(block.timestamp)
+                })
+            );
+
+            (uint amount0CollectedFromPosition, uint amount1CollectedFromPosition) = univ3NFPM.collect(
+                INonfungiblePositionManager.CollectParams({
+                    tokenId: tokenIds[i],
+                    recipient: address(this),
+                    amount0Max: liquidity,
+                    amount1Max: liquidity
+                })
+            );
+
+            amount0Collected += amount0CollectedFromPosition;
+            amount1Collected += amount1CollectedFromPosition;
+
+            univ3NFPM.burn(tokenIds[i]);
+
+            unchecked { ++i; }
+        }
+
+        // TODO: I think the best way to get the collateral tracker for each token is to get the Uniswap pool for the pair of tokens, and then get Panoptic pool for the uniswap pool. Henry mentioned something about naming - is there a more efficient way to get the tracker from the name alone? I did not see one.
+        PanopticPool pool = panopticFactory.getPanopticPool(
+            univ3Factory.getPool(token0, token1)
+        );
+
+        CollateralTracker ct0 = pool.collateralToken0();
+        IERC20(token0).approve(ct0, amount0Collected);
+        ct0.deposit(amount0Collected, msg.sender);
+
+        CollateralTracker ct1 = pool.collateralToken1();
+        IERC20(token1).approve(ct1, amount1Collected);
+        ct1.deposit(amount1Collected, msg.sender);
+    }
+
+    // TODO: alternative .migrate that takes in an array of pairs of amount0Min and amount1Min so you can ensure the tokens you pull out of uniswap and put into the PLP are at the ratio you desire (IE: slippage protection).
+    // same logic as the above .migrate but passes in minimums[i].amount0/1Min in the .decreaseLiquidity call
+    function migrate(uint256 tokenIds, uint256[][] amountMins) external { }
+}
---
+++ test/foundry/periphery/UniswapMigrator.t.sol
@@ -0,0 +1,193 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {UniswapMigrator} from "@periphery/UniswapMigrator.sol";
+import {PanopticPool} from "@contracts/PanopticPool.sol";
+import {PanopticMath} from "@contracts/libraries/PanopticMath.sol";
+import {PanopticFactory} from "@contracts/PanopticFactory.sol";
+import {CollateralTracker} from "@contracts/CollateralTracker.sol";
+import {IDonorNFT} from "@tokens/interfaces/IDonorNFT.sol";
+import {DonorNFT} from "@periphery/DonorNFT.sol";
+import {SemiFungiblePositionManager} from "@contracts/SemiFungiblePositionManager.sol";
+// and with UniswapMigration should come PanopticFactory, PanopticPool, IUniswapV3Factory, INonfungiblePositionManager and IERC20
+import {IUniswapV3Pool} from "v3-core/interfaces/IUniswapV3Pool.sol";
+import {IUniswapV3Factory} from "v3-core/interfaces/IUniswapV3Factory.sol";
+import {INonfungiblePositionManager} from "v3-periphery/interfaces/INonfungiblePositionManager.sol";
+
+import {IERC20Partial} from "@tokens/interfaces/IERC20Partial.sol";
+
+
+contract SemiFungiblePositionManagerHarness is SemiFungiblePositionManager {
+    /// TODO I think the vanilla SFPM is actually all this test needs, but if you need, check out the harness in PanopticHelper.t.sol
+}
+
+contract PanopticPoolHarness is PanopticPool {
+    /// TODO I think the vanilla Pool is actually all this test needs, but if you need, check out the positions hash getter and other things in the harness inside of PanopticHelper.t.sol
+}
+
+contract UniswapMigratorTest {
+    SemiFungiblePositionManagerHarness sfpm;
+
+    IUniswapV3Factory V3FACTORY = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
+    INonfungiblePositionManager V3NFPM = INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
+
+    PanopticFactory factory;
+
+    // store a few different mainnet pairs - the pool used is part of the fuzz
+    IUniswapV3Pool constant USDC_WETH_5 =
+        IUniswapV3Pool(0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640);
+    IUniswapV3Pool constant WBTC_ETH_30 =
+        IUniswapV3Pool(0xCBCdF9626bC03E24f779434178A73a0B4bad62eD);
+    IUniswapV3Pool constant USDC_WETH_30 =
+        IUniswapV3Pool(0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8);
+    IUniswapV3Pool[3] public pools = [USDC_WETH_5, USDC_WETH_5, USDC_WETH_5];
+
+    address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    // store some data about the pool we are testing
+    IUniswapV3Pool pool;
+    uint64 poolId;
+    address token0;
+    address token1;
+    // We range position size in terms of WETH, so need to figure out which token is WETH
+    uint256 isWETH;
+    uint24 fee;
+    int24 tickSpacing;
+    uint160 currentSqrtPriceX96;
+    int24 currentTick;
+    uint256 feeGrowthGlobal0X128;
+    uint256 feeGrowthGlobal1X128;
+    uint256 poolBalance0;
+    uint256 poolBalance1;
+
+    address Deployer = address(0x1234);
+    address LiquidityProvider = address(0x123456);
+
+    PanopticPoolHarness pp;
+    CollateralTracker ct0;
+    CollateralTracker ct1;
+
+    // reference implemenatations used by the factory
+    address poolReference;
+    address collateralReference;
+
+    uint256 liquidityProvisionTokenId;
+
+    UniswapMigrator uniswapMigrator;
+
+    function _initPool(uint256 seed) internal {
+        _initWorld(seed);
+    }
+
+    function _initWorld(uint256 seed) internal {
+        // Pick a pool from the seed and cache initial state
+        _cacheWorldState(pools[bound(seed, 0, pools.length - 1)]);
+
+        _deployPanopticPool();
+
+        _initAccounts();
+    }
+
+    function _cacheWorldState(IUniswapV3Pool _pool) internal {
+        pool = _pool;
+        poolId = PanopticMath.getPoolId(address(_pool));
+        token0 = _pool.token0();
+        token1 = _pool.token1();
+        isWETH = token0 == address(WETH) ? 0 : 1;
+        fee = _pool.fee();
+        tickSpacing = _pool.tickSpacing();
+        (currentSqrtPriceX96, currentTick, , , , , ) = _pool.slot0();
+        feeGrowthGlobal0X128 = _pool.feeGrowthGlobal0X128();
+        feeGrowthGlobal1X128 = _pool.feeGrowthGlobal1X128();
+        poolBalance0 = IERC20Partial(token0).balanceOf(address(_pool));
+        poolBalance1 = IERC20Partial(token1).balanceOf(address(_pool));
+    }
+
+    function _deployPanopticPool() internal {
+        vm.startPrank(Deployer);
+
+        IDonorNFT dNFT = IDonorNFT(address(new DonorNFT()));
+
+        factory = new PanopticFactory(
+            WETH,
+            sfpm,
+            V3FACTORY,
+            dNFT,
+            poolReference,
+            collateralReference
+        );
+
+        factory.initialize(Deployer);
+
+        DonorNFT(address(dNFT)).changeFactory(address(factory));
+
+        deal(token0, Deployer, type(uint104).max);
+        deal(token1, Deployer, type(uint104).max);
+        IERC20Partial(token0).approve(address(factory), type(uint104).max);
+        IERC20Partial(token1).approve(address(factory), type(uint104).max);
+
+        pp = PanopticPoolHarness(
+            address(
+                factory.deployNewPool(
+                    token0,
+                    token1,
+                    fee,
+                    bytes32(uint256(uint160(Deployer)) << 96)
+                )
+            )
+        );
+
+        ct0 = pp.collateralToken0();
+        ct1 = pp.collateralToken1();
+    }
+
+    function _initAccounts() internal {
+        // TODO: Deal some tokens to people in here; see PanopticHelper.t.sol 's _initAccounts for inspiration
+    }
+
+    function setUp() public {
+        sfpm = new SemiFungiblePositionManagerHarness(V3FACTORY);
+
+        // deploy reference pool and collateral token
+        poolReference = address(new PanopticPoolHarness(sfpm));
+        collateralReference = address(
+            new CollateralTracker(10, 2_000, 1_000, -1_024, 5_000, 9_000, 20_000)
+        );
+
+        // set up the liquidity provider's LP position
+        deal(token0, LiquidityProvider, type(uint104).max);
+        deal(token1, LiquidityProvider, type(uint104).max);
+        vm.prank(LiquidityProvider);
+        IERC20Partial(token0).approve(address(V3NFPM), type(uint104).max);
+        IERC20Partial(token1).approve(address(V3NFPM), type(uint104).max);
+
+        (liquidityProvisionTokenId,,,) = V3NFPM.mint(INonfungiblePositionManager.MintParams({
+            token0: token0,
+            token1: token1,
+            // TODO: Do these need to be sensical values, or can I just put 1 for all them?
+            // I see the NonfungiblePositionManager test from Uniswap actually calls a getMinTick / getMaxTick when calling .mint in its tests, but i don't know if that's necessary
+            fee: 1,
+            tickLower: 1,
+            tickUpper: 1,
+            amount0Desired: 1,
+            amount1Desired: 1,
+            amount0Min: 1,
+            amount1Min: 1,
+            recipient: LiquidityProvider,
+            deadline: block.timestamp
+        }));
+
+        // and finally, deploy the migrator!
+        uniswapMigrator = new UniswapMigrator(
+            factory,
+            V3NFPM,
+            V3FACTORY
+        );
+    }
+
+    function test_migrateSuccess() public {
+        vm.prank(LiquidityProvider);
+        uniswapMigrator.migrate([liquidityProvisionTokenId]);
+    }
+}
