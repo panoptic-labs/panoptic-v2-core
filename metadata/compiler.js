748b4039323db3f291394ff13fac3d50c0a2dbcf
dyedm1 <henry@hyf.one>
2024-05-20 12:06:23 -0400

    feat: add compilation settings for metadata contracts

--- metadata/compiler.js
+++ metadata/compiler.js
@@ -15,57 +15,45 @@ let pointers = []
 // The resulting structure is K_1 -> K_2 -> V where K_1 is a property name, K_2 is an index, and V is a pointer to the value in the contract
 // If the value of a top-level key is an array, the indices correspond to the array indices
 // If the second level of the JSON is an object, the indices are encoded key strings
-for (const [key, value] of Object.entries(data)) {
+for (const [key, value] of Object.entries(data.metadata)) {
     properties.push(key)
     indices.push([])
     pointers.push([])
 
-    // 
     if (Array.isArray(value)) {
         value.forEach((v, idx) => {
             indices[indices.length - 1].push(idx.toString())
 
-            let encoded = ""
-
-            // compress large values using LZ77
-            if (key == "art" || key == "frames" || key == "filters" || key == "descriptions") encoded = LibZip.flzCompress("0x"+Buffer.from(new TextEncoder().encode(v)).toString('hex')).slice(2)
-            else encoded = encodeBytes(v)
-
-            // contract size (minus initcode) cannot exceed the Spurious Dragon limit, so complete the current contract and place `encoded` in a new contract
-            if (bytecodes[bytecodes.length - 1].length + encoded.length > 24576 * 2) {
-                prependInitcode()
-
-                bytecodes.push(encoded)
-                pointers[indices.length - 1].push({"start": 0, "size": encoded.length / 2, "codeIndex": bytecodes.length - 1})
-            } else {
-                bytecodes[bytecodes.length - 1] += encoded
-                pointers[indices.length - 1].push({"start": bytecodes[bytecodes.length - 1].length / 2 - encoded.length / 2, "size": encoded.length / 2, "codeIndex": bytecodes.length - 1})
-            }
+            // compress large values using LZ77 if specified in the settings
+            appendBytes(data.settings.compressedProps.includes(key) ? LibZip.flzCompress("0x"+Buffer.from(new TextEncoder().encode(v)).toString('hex')).slice(2) : encodeBytes(v))
         })
     } else {
         for (const [k, v] of Object.entries(value)) {
             indices[indices.length - 1].push(BigInt("0x"+Buffer.from(new TextEncoder().encode(k)).toString('hex').padEnd(64, '0')).toString())
 
-            const encoded = encodeBytes(v)
-
-            // contract size (minus initcode) cannot exceed the Spurious Dragon limit, so complete the current contract and place `encoded` in a new contract
-            if (bytecodes[bytecodes.length - 1].length + encoded.length > 24576 * 2) {
-                prependInitcode()
-
-                bytecodes.push(encoded)
-                pointers[indices.length - 1].push([{"start": 0, "size": encoded.length / 2, "codeIndex": bytecodes.length - 1}])
-            } else {
-                bytecodes[bytecodes.length - 1] += encoded
-                pointers[indices.length - 1].push({"start": bytecodes[bytecodes.length - 1].length / 2 - encoded.length / 2, "size": encoded.length / 2, "codeIndex": bytecodes.length - 1})
-            }
+            appendBytes(encodeBytes(v))
         }
     }
 }
 
 // complete the last contract
-if (bytecodes[bytecodes.length - 1].length < (24576 + 14) * 2) prependInitcode()
+prependInitcode()
 
 Bun.write('./metadata/out/MetadataPackage.json', JSON.stringify({"bytecodes": bytecodes, "properties": properties, "indices": indices, "pointers": pointers}))
+
+// append bytes to the data stored in `bytecodes`, creating a new contract if necessary
+function appendBytes(bytes) {
+    // contract size (minus initcode) cannot exceed the contract size limit (if applicable), so complete the current contract and place `encoded` in a new contract
+    if (bytecodes[bytecodes.length - 1].length + bytes.length > data.settings.maxCodeSize * 2) {
+        prependInitcode()
+
+        bytecodes.push(bytes)
+        pointers[indices.length - 1].push({"start": 0, "size": bytes.length / 2, "codeIndex": bytecodes.length - 1})
+    } else {
+        bytecodes[bytecodes.length - 1] += bytes
+        pointers[indices.length - 1].push({"start": bytecodes[bytecodes.length - 1].length / 2 - bytes.length / 2, "size": bytes.length / 2, "codeIndex": bytecodes.length - 1})
+    }
+}
 
 // prepend initcode to last item in bytecode array with its final size
 // this code copies the rest of the bytecode (encoded data) and returns it, resulting in a deplyoed contract with the desired data encoded in the "code"
