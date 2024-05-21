9e9bb17b0ffecbc58d07787cd07c04c919be6a7d
dyed <henry@hyf.one>
2024-05-21 08:55:57 -0400

    fix: prevent users from purchasing all liquidity in a chunk  (#66)
    * fix: refrain from referring to `removedLiquidity` as `totalLiquidity`
    
    * fix: prevent users from purchasing all liquidity in a chunk (#56)

--- test/foundry/core/Misc.t.sol
+++ test/foundry/core/Misc.t.sol
@@ -484,6 +484,50 @@ contract Misctest is Test, PositionUtils {
         assertEq(ct0.previewMint(ct0.maxMint(Alice)), type(uint104).max);
     }
 
+    function test_fail_buyAllLiquidity() public {
+        swapperc = new SwapperC();
+        vm.startPrank(Swapper);
+        token0.mint(Swapper, type(uint128).max);
+        token1.mint(Swapper, type(uint128).max);
+        token0.approve(address(swapperc), type(uint128).max);
+        token1.approve(address(swapperc), type(uint128).max);
+
+        // mint OTM position
+        $posIdList.push(
+            TokenId.wrap(0).addPoolId(PanopticMath.getPoolId(address(uniPool))).addLeg(
+                0,
+                1,
+                1,
+                0,
+                0,
+                0,
+                15,
+                1
+            )
+        );
+
+        $tempIdList.push(
+            TokenId.wrap(0).addPoolId(PanopticMath.getPoolId(address(uniPool))).addLeg(
+                0,
+                1,
+                1,
+                1,
+                0,
+                0,
+                15,
+                1
+            )
+        );
+
+        vm.startPrank(Alice);
+        pp.mintOptions($posIdList, 1_000_000, 0, 0, 0);
+
+        vm.startPrank(Bob);
+
+        vm.expectRevert(stdError.divisionError);
+        pp.mintOptions($tempIdList, 1_000_000, 0, 0, 0);
+    }
+
     // these tests are PoCs for rounding issues in the premium distribution
     // to demonstrate the issue log the settled, gross, and owed premia at burn
     function test_settledPremiumDistribution_demoInflatedGross() public {
