ac1e9fbf726e0da7a564af6a6ea43771c2e68cde
0xTARC <0xtarc@protonmail.com>
2024-06-24 23:44:31 +0200

    fix: update deploy script

--- deploy/DeployProtocol.s.sol
+++ deploy/DeployProtocol.s.sol
@@ -7,11 +7,16 @@ import "forge-std/Script.sol";
 import {IUniswapV3Factory} from "v3-core/interfaces/IUniswapV3Factory.sol";
 // Internal
 import {CollateralTracker} from "@contracts/CollateralTracker.sol";
-import {IDonorNFT} from "@tokens/interfaces/IDonorNFT.sol";
-import {DonorNFT} from "@periphery/DonorNFT.sol";
 import {PanopticPool} from "@contracts/PanopticPool.sol";
 import {SemiFungiblePositionManager} from "@contracts/SemiFungiblePositionManager.sol";
 import {PanopticFactory} from "@contracts/PanopticFactory.sol";
+import {Pointer, PointerLibrary} from "@types/Pointer.sol";
+
+struct PointerInfo {
+    uint256 codeIndex;
+    uint256 end;
+    uint256 start;
+}
 
 /**
  * @title Deployment script that deploys PanopticFactory, SFPM, and dependencies
@@ -38,17 +43,73 @@ contract DeployProtocol is Script {
             new CollateralTracker(10, 2_000, 1_000, -1_024, 5_000, 9_000, 20_000)
         );
 
-        IDonorNFT dNFT = IDonorNFT(address(new DonorNFT()));
+        // Read metadata for deployment
+        string memory metadata = vm.readFile("./metadata/out/MetadataPackage.json");
+
+        // Parse bytecodes from metadata
+        bytes[] memory bytecodes = vm.parseJsonBytesArray(metadata, ".bytecodes");
+        address[] memory pointerAddresses = new address[](bytecodes.length);
+
+        // Deploy bytecodes and store pointers
+        for (uint256 i = 0; i < bytecodes.length; i++) {
+            bytes memory code = bytecodes[i];
+            address pointer;
+            assembly {
+                pointer := create(0, add(code, 0x20), mload(code))
+                if iszero(extcodesize(pointer)) {
+                    revert(0, 0)
+                }
+            }
+            pointerAddresses[i] = pointer;
+        }
+
+        // Decode pointers from metadata
+        PointerInfo[][] memory pointerInfo = abi.decode(
+            vm.parseJson(metadata, ".pointers"),
+            (PointerInfo[][])
+        );
+        Pointer[][] memory pointers = new Pointer[][](pointerInfo.length);
+
+        // Create pointers from decoded information
+        for (uint256 i = 0; i < pointerInfo.length; i++) {
+            pointers[i] = new Pointer[](pointerInfo[i].length);
+            for (uint256 j = 0; j < pointerInfo[i].length; j++) {
+                pointers[i][j] = PointerLibrary.createPointer(
+                    pointerAddresses[pointerInfo[i][j].codeIndex],
+                    uint48(pointerInfo[i][j].start),
+                    uint48(pointerInfo[i][j].end)
+                );
+            }
+        }
+
+        // Parse properties from metadata
+        string[] memory propsStr = vm.parseJsonStringArray(metadata, ".properties");
+        bytes32[] memory props = new bytes32[](propsStr.length);
+        for (uint256 i = 0; i < propsStr.length; i++) {
+            props[i] = bytes32(bytes(propsStr[i]));
+        }
+
+        // Decode and parse indices from metadata
+        string[][] memory indicesStr = abi.decode(vm.parseJson(metadata, ".indices"), (string[][]));
+        uint256[][] memory indices = new uint256[][](indicesStr.length);
+        for (uint256 i = 0; i < indicesStr.length; i++) {
+            indices[i] = new uint256[](indicesStr[i].length);
+            for (uint256 j = 0; j < indicesStr[i].length; j++) {
+                indices[i][j] = vm.parseUint(indicesStr[i][j]);
+            }
+        }
+
+        // Deploy the PanopticFactory with the new constructor parameters
         PanopticFactory factory = new PanopticFactory(
             WETH9,
             SFPM,
             UNISWAP_V3_FACTORY,
-            dNFT,
             poolReference,
-            collateralReference
+            collateralReference,
+            props,
+            indices,
+            pointers
         );
-
-        DonorNFT(address(dNFT)).changeFactory(address(factory));
 
         vm.stopBroadcast();
     }
