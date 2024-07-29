38fa44427fba100705f43ca966b76a26a3691214
Steve Flanagan <flanagan.s@husky.neu.edu>
2024-07-29 17:36:00 -0400

    feat: skip known contract problem + consider the rounding of s_grossPremiumLast

--- contracts/fuzz/FuzzDeployments.sol
+++ contracts/fuzz/FuzzDeployments.sol
@@ -1844,30 +1844,48 @@ contract FuzzDeployments is FuzzHelpers {
             // If we don't get that, then we're not working properly.
             // NOTE: 7/27 - Confirmed, we got this! Can feel free to modify this test to specifically
             // skip those cases if you want to see other possible invariant failures
-            assertWithMsg(
-                (($postburnSFPMGrossPremiaAccumulator0 - $postburnGrossPremiaLast0) *
-                    $postburnShortLiquidity) >>
-                    64 ==
-                    (uint256(
-                        (uint256(preburnAccumulators[legIndex].sfpmGrossPremiaAccumulator0) -
-                            uint256(preburnAccumulators[legIndex].grossPremiaLast0)) *
-                            uint256(preburnAccumulators[legIndex].totalShortLiquidity)
-                    ) >> 64) -
-                        $projectedPremia[legIndex].idealPremium0,
-                "grossPremiaLast on token0 did not go down by the total amount of premia owed for the now-burnt position"
-            );
-            assertWithMsg(
-                (($postburnSFPMGrossPremiaAccumulator1 - $postburnGrossPremiaLast1) *
-                    $postburnShortLiquidity) >>
-                    64 ==
-                    (uint256(
-                        (uint256(preburnAccumulators[legIndex].sfpmGrossPremiaAccumulator1) -
-                            uint256(preburnAccumulators[legIndex].grossPremiaLast1)) *
-                            uint256(preburnAccumulators[legIndex].totalShortLiquidity)
-                    ) >> 64) -
-                        $projectedPremia[legIndex].idealPremium1,
-                "grossPremiaLast on token1 did not go down by the total amount of premia owed for the now-burnt position"
-            );
+            // NOTE: 7/29 - Added that skip as an `if` before each assertion:
+            if (!($projectedPremia[legIndex].idealPremium0 == 0 && $position.isLong(legIndex) == 0)) {
+                uint256 projectedPostburnGrossPremia0 = (
+                        uint256(
+                            (
+                                uint256(preburnAccumulators[legIndex].sfpmGrossPremiaAccumulator0) -
+                                uint256(preburnAccumulators[legIndex].grossPremiaLast0)
+                            )
+                            * uint256(preburnAccumulators[legIndex].totalShortLiquidity)
+                        ) >> 64
+                    ) - $projectedPremia[legIndex].idealPremium0;
+                uint256 actualPostburnGrossPremia0 = (
+                    ($postburnSFPMGrossPremiaAccumulator0 - $postburnGrossPremiaLast0)
+                    * $postburnShortLiquidity
+                ) >> 64;
+                assertWithMsg(
+                    // Tolerance of 1 as the contracts round s_grossPremiumLast down,
+                    // and our idealPremium calc may not
+                    actualPostburnGrossPremia0 - projectedPostburnGrossPremia0 <= 1,
+                    "grossPremiaLast on token0 did not go down by the total amount of premia owed for the now-burnt position"
+                );
+            }
+
+            if (!($projectedPremia[legIndex].idealPremium1 == 0 && $position.isLong(legIndex) == 0)) {
+                uint256 projectedPostburnGrossPremia1 = (
+                        uint256(
+                            (
+                                uint256(preburnAccumulators[legIndex].sfpmGrossPremiaAccumulator1) -
+                                uint256(preburnAccumulators[legIndex].grossPremiaLast1)
+                            )
+                            * uint256(preburnAccumulators[legIndex].totalShortLiquidity)
+                        ) >> 64
+                    ) - $projectedPremia[legIndex].idealPremium1;
+                uint256 actualPostburnGrossPremia1 = (
+                    ($postburnSFPMGrossPremiaAccumulator1 - $postburnGrossPremiaLast1)
+                    * $postburnShortLiquidity
+                ) >> 64;
+                assertWithMsg(
+                    actualPostburnGrossPremia1 - projectedPostburnGrossPremia1 <= 1,
+                    "grossPremiaLast on token1 did not go down by the total amount of premia owed for the now-burnt position"
+                );
+            }
             delete $expectedSettledToken0DifferenceForChunk;
             delete $expectedSettledToken1DifferenceForChunk;
             delete $postburnSettledToken0;
