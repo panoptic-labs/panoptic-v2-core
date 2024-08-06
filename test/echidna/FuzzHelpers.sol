201946cb226a6e7289179346dbf02577bef33a5e
Lint Action <devs@panoptic.xyz>
2024-08-06 19:31:23 +0000

    Fix code style issues with Prettier

--- test/echidna/FuzzHelpers.sol
+++ test/echidna/FuzzHelpers.sol
@@ -2017,14 +2017,14 @@ contract FuzzHelpers is PropertiesAsserts {
                             ($tokenTypes[i] == 1 && $fastOracleTick <= 0))
                             ? baseCR
                             : $tokenTypes[i] == 0
-                                ? PanopticMath.convert0to1(
-                                    baseCR,
-                                    Math.getSqrtRatioAtTick($fastOracleTick)
-                                )
-                                : PanopticMath.convert1to0(
-                                    baseCR,
-                                    Math.getSqrtRatioAtTick($fastOracleTick)
-                                )
+                            ? PanopticMath.convert0to1(
+                                baseCR,
+                                Math.getSqrtRatioAtTick($fastOracleTick)
+                            )
+                            : PanopticMath.convert1to0(
+                                baseCR,
+                                Math.getSqrtRatioAtTick($fastOracleTick)
+                            )
                     ) * 13_333) / 10_000) * $ratios[i]
                 );
                 emit LogUint256("2", 2);
@@ -2109,14 +2109,14 @@ contract FuzzHelpers is PropertiesAsserts {
                                 ($tokenTypes[i] == 1 && $fastOracleTick <= 0))
                                 ? ITMCR
                                 : $tokenTypes[i] == 0
-                                    ? PanopticMath.convert0to1(
-                                        ITMCR,
-                                        Math.getSqrtRatioAtTick($fastOracleTick)
-                                    )
-                                    : PanopticMath.convert1to0(
-                                        ITMCR,
-                                        Math.getSqrtRatioAtTick($fastOracleTick)
-                                    )
+                                ? PanopticMath.convert0to1(
+                                    ITMCR,
+                                    Math.getSqrtRatioAtTick($fastOracleTick)
+                                )
+                                : PanopticMath.convert1to0(
+                                    ITMCR,
+                                    Math.getSqrtRatioAtTick($fastOracleTick)
+                                )
                         ) * 13_333) / 10_000) * $ratios[i]
                     );
                 }
