af5ccef9b5a48362803df93f584e44cda9438734
dyedm1 <henry@hyf.one>
2024-10-09 20:11:12 +0000

    fix: rm unused import

--- contracts/types/LiquidityChunk.sol
+++ contracts/types/LiquidityChunk.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.24;
 
-// Custom types
-import {TokenId} from "@types/TokenId.sol";
-
 type LiquidityChunk is uint256;
 using LiquidityChunkLibrary for LiquidityChunk global;
 
