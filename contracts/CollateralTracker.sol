5e574a9be32ec4b85530e6c26378284a45157132
Guillaume Lambert <guil.lambert@protonmail.com>
2025-10-04 23:01:57 -0400

    feat: explicitly prevent zero amountMoved positions

--- contracts/CollateralTracker.sol
+++ contracts/CollateralTracker.sol
@@ -1505,6 +1505,8 @@ contract CollateralTracker is ERC20Minimal, Multicall {
         // amount moved is right slot if tokenType=0, left slot otherwise
         uint128 amountMoved = tokenType == 0 ? amountsMoved.rightSlot() : amountsMoved.leftSlot();
 
+        if (amountMoved == 0) revert Errors.ZeroLiquidity();
+
         uint256 isLong = tokenId.isLong(index);
 
         // start with base requirement, which is based on isLong value
