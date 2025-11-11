4d80ea259ff19ebd8f95b0161007bc2ef6feef06
guil-lambert <83659385+guil-lambert@users.noreply.github.com>
2025-11-10 23:25:22 -0500

    Docs: [I-01]  Incorrect comment in PanopticFactory.deployNewPool() (#193)
    * fix: use correct bitshift and mine address salt structure
    
    * test: adjusted the tests
    
    * docs: updated comment in deployNewPool()

--- contracts/PanopticFactory.sol
+++ contracts/PanopticFactory.sol
@@ -136,7 +136,7 @@ contract PanopticFactory is FactoryNFT, Multicall {
         uint64 poolId = SFPM.initializeAMMPool(token0, token1, fee);
 
         // Users can specify a salt, the aim is to incentivize the mining of addresses with leading zeros
-        // salt format: (first 20 characters of deployer address) + (first 20 characters of UniswapV3Pool) + (uint96 user supplied salt)
+        // salt format: (first 20 characters of deployer address) + (first 10 characters of UniswapV3Pool) + (first 10 characters of RiskEngine) + (uint96 user supplied salt)
         bytes32 salt32 = bytes32(
             abi.encodePacked(
                 uint80(uint160(msg.sender) >> 80),
