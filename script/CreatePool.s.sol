829b8932e4871b1981e738c3c678af3f5acd96bb
zjesko <akshat.c2k@gmail.com>
2026-01-13 00:59:44 -0500

    feat: create pool script

---
+++ script/CreatePool.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Foundry
+import "forge-std/Script.sol";
+import {PanopticFactory} from "@contracts/PanopticFactory.sol";
+import {PanopticPool} from "@contracts/PanopticPool.sol";
+import {IRiskEngine} from "@contracts/interfaces/IRiskEngine.sol";
+import {IUniswapV3Pool} from "univ3-core/interfaces/IUniswapV3Pool.sol";
+
+contract CreatePool is Script {
+    function run() public {
+        uint256 DEPLOYER_PRIVATE_KEY = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        PanopticFactory factory = PanopticFactory(vm.envAddress("PANOPTIC_FACTORY"));
+        IRiskEngine riskEngine = IRiskEngine(vm.envAddress("RISK_ENGINE"));
+
+        // WETH/USDC 500 bps pool on Sepolia
+        address WETH_USDC_500_POOL = 0x1105514b9Eb942F2596A2486093399b59e2F23fC;
+
+        // Get token addresses from the pool
+        IUniswapV3Pool v3Pool = IUniswapV3Pool(WETH_USDC_500_POOL);
+        address token0 = v3Pool.token0();
+        address token1 = v3Pool.token1();
+        uint24 fee = v3Pool.fee();
+
+        console.log("Creating Panoptic Pool for WETH/USDC 500 bps");
+        console.log("Uniswap V3 Pool:", WETH_USDC_500_POOL);
+        console.log("Token0:", token0);
+        console.log("Token1:", token1);
+        console.log("Fee:", fee);
+        console.log("Factory:", address(factory));
+        console.log("RiskEngine:", address(riskEngine));
+
+        vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
+
+        uint96 salt = 0;
+
+        PanopticPool newPool = factory.deployNewPool(token0, token1, fee, riskEngine, salt);
+
+        console.log("Successfully deployed Panoptic Pool at:", address(newPool));
+
+        vm.stopBroadcast();
+    }
+}
